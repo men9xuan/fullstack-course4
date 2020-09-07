ed11fc9e58d15bad70922e1aeeed326a85c17154
men9xuan <mengxuanshi@Zombie.hitronhub.home>
2020-09-07 02:31:33 -0400

    edited by practice

--- examples/Lecture42/js/script.js
+++ examples/Lecture42/js/script.js
@@ -2,15 +2,14 @@
 var x;
 console.log(x);
 
-if (x == undefined) {
-  console.log("x is undefined");
-}
+// x =5;
+// if (x == undefined) {
+//   console.log("x is undefined");
+// }
 
-x = 5;
-if (x == undefined) {
-  console.log("x is undefined");
-}
-else {
-  console.log("x has been defined");
-}
-
+// if (x == undefined) {
+//   console.log("x is undefined");
+// }
+// else {
+//   console.log("x has been defined");
+// }
